9458566c3bf7fcb662e9d3ceb445c1d61f422e63
yukiny0811 <yukiny0811@gmail.com>
2024-05-11 13:32:03 +0900

    Initial Commit

---
+++ TalkDeck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TalkDeck
+//
+//  Created by Yuki Kuwashima on 2024/05/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TalkDeck/TalkDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkDeckApp.swift
+//  TalkDeck
+//
+//  Created by Yuki Kuwashima on 2024/05/11.
+//
+
+import SwiftUI
+
+@main
+struct TalkDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
